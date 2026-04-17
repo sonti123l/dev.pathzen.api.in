55662876170f6cf3245ade96e1d2421f747a9d40
SaiTrishal22 <saitrishal.s@orotron.com>
2026-04-17 14:20:43 +0530

    user not present condition is added

--- src/db/schema/courses.ts
+++ src/db/schema/courses.ts
@@ -11,7 +11,7 @@ export const courses = mysqlTable("courses", {
   course_id: int("course_id").primaryKey().notNull().autoincrement(),
   course_name: varchar("course_name", { length: 255 }).notNull(),
   course_meta_data: json("course_meta_data"),
-  course_created_at: timestamp("course_created_at").defaultNow(),
+  course_created_at: timestamp("course_created_at").defaultNow().notNull(),
 
   field_id: int("field_id").references(() => domains.domain_id)
 });
