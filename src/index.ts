adb839198b8cdd988a9a3605eacee903f4048410
SaiTrishal22 <saitrishal.s@orotron.com>
2026-04-30 04:07:39 +0530

    host name changed

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,8 @@ app.use(
 serve(
   {
     fetch: app.fetch,
-    port: 3000,
+    port: Number(process.env.PORT) || 3000,
+    hostname: '0.0.0.0'
   },
   (info) => {
     console.log(`Server is running on http://localhost:${info.port}`);
