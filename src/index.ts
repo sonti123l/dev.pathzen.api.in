5a857ec548d2ce8c038753dd049c0c07667f5b34
SaiTrishal22 <saitrishal.s@orotron.com>
2026-05-16 10:54:39 +0530

    Added jwt token

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { authRouter } from "./routes/authRouter.js";
-import { jwt } from "hono/jwt";
 import "dotenv/config";
 import { resourceRouter } from "./routes/resourceRouter.js";
 import appRouter from "./routes/appRouter.js";
 import roomRouter from "./routes/roomRouter.js";
+import checkAuthorization from "./middleware/middleware.js";
 
 const app = new Hono();
 
@@ -16,41 +16,23 @@ app.get("/", (c) => {
 
 app.use("*", cors());
 
-// Protect only API and course routes — login/register do NOT need a token
-app.use("/api/*", jwt({
-  secret: `${process.env.JWT_ACCESS_SECRET_KEY}`,
-  alg: "HS256",
-}));
-
-app.use("/course/*", jwt({
-  secret: `${process.env.JWT_ACCESS_SECRET_KEY}`,
-  alg: "HS256",
-}));
-
-app.use("/rooms/*", jwt({
-  secret: `${process.env.JWT_ACCESS_SECRET_KEY}`,
-  alg: "HS256",
-}));
-
 app.route("/auth", authRouter);
 app.route("/api", resourceRouter);
+
+app.use("/course/*", checkAuthorization);
+app.use("/rooms/*", checkAuthorization);
+
 app.route("/course", appRouter);
 app.route("/rooms", roomRouter);
-
-app.get("/test-env", (c) => {
-  return c.json({
-    cloudflare_account_id: process.env.CLOUDFLARE_ACCOUNT_ID ? "exists" : "missing",
-    cloudflare_api_token: process.env.CLOUDFLARE_API_TOKEN ? "exists" : "missing",
-  })
-})
 
 serve(
   {
     fetch: app.fetch,
     port: Number(process.env.PORT) || 3000,
-    hostname: '0.0.0.0'
+    hostname: "0.0.0.0",
   },
   (info) => {
     console.log(`Server is running on http://localhost:${info.port}`);
   },
 );
+
